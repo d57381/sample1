dd629d91dcd551278083ec290ac2ae952f9ee97a
d57381 <dreinhart.81@gmail.com>
2022-09-14 14:47:18 -0400

    Implemented multiplication API

--- src/cljs/sample1/core.cljs
+++ src/cljs/sample1/core.cljs
@@ -39,30 +39,37 @@
   [:section.section>div.container>div.content
    [:img {:src "/img/warning_clojure.png"}]])
 
-(def getAtom (r/atom 0))
-(def postAtom (r/atom 0))
+(def getAtom (r/atom 0)) ;Atom that will hold the value obtained from GET
+(def postAtom (r/atom 0)) ;Atom that will hold the value obtained from POST
+(def multAtom (r/atom 0)) ;Atom that will hold the value for multiplication
 
 (def toAdd [(rand-int 1000) (rand-int 1000)])
 ;(def comps [(r/atom comp1 [1 2 0]) (r/atom comp2 [97 52 0]) (r/atom comp3 [2 2 0])])
 
-(defn postSum [vec resAtom]
+(defn postSum [vec resultAtom]
   (POST "/api/math/plus"    {:headers {"accept" "application/json"}
                              :params {:x (first vec) :y (second vec)}
-                             :handler #(reset! resAtom (:total %))})
+                             :handler #(reset! resultAtom (:total %))})
   )
 
-(defn getSum [vec resAtom]
+(defn getSum [vec resultAtom]
   (GET "/api/math/plus"    {:headers {"accept" "application/json"}
                                          :params {:x (first vec) :y (second vec)}
-                                         :handler #(reset! resAtom (:total %))}
+                                         :handler #(reset! resultAtom (:total %))}
 
     )
   )
 
+(defn postProduct [vec resultAtom]
+  (POST "/api/math/times" {:headers {"accept" "application/json"}
+                           :params  {:x (first vec) :y (second vec)}
+                           :handler #(reset! resultAtom (:total %))}))
+
 (defn home-page []
 
   [:p [:p "Using GET, the sum of " (first toAdd) " and " (second toAdd) " is: " @getAtom]
-   [:p "Using POST, the sum of " (first toAdd) " and " (second toAdd) " is: " @postAtom]]
+   [:p "Using POST, the sum of " (first toAdd) " and " (second toAdd) " is: " @postAtom]
+   [:p "The product of " (first toAdd) " and " (second toAdd) " is " @multAtom]]
 
   )
 
@@ -113,6 +120,7 @@
 
   (postSum toAdd postAtom)
   (getSum toAdd getAtom)
+  (postProduct toAdd multAtom)
 
   (hook-browser-navigation!)
   (mount-components))
